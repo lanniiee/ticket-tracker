edecf4c346a78a2d9a193356d2b749829253778f
lanniiee <lav250397@hotmail.com>
2022-10-20 17:46:00 +0100

    create new files

---
+++ src/data/team.js
@@ -0,0 +1,63 @@
+const team = [
+    {
+      id: 1,
+      name: "Alima Miller",
+      role: "Junior Software Developer",
+      imgUrl: "https://hips.hearstapps.com/hbu.h-cdn.co/assets/16/14/2048x1024/1459784765-landscape-1459451893-gettyimages-143061326.jpg?resize=980:*"
+    },
+    {
+      id: 2,
+      name: "Evelyn Rodgers",
+      role: "Junior Software Developer",
+      imgUrl: "https://images.prestigeonline.com/wp-content/uploads/sites/6/2022/04/11125829/AdobeStock_292786948.jpg"
+    },
+    {
+      id: 3,
+      name: "Henley Myers",
+      role: "Junior Software Developer",
+      imgUrl: "https://media.worldnomads.com/explore/vietnam/halong-bay-vietnam-from-above-gettyimages.jpg"
+    },
+    {
+      id: 4,
+      name: "Celyn Sanders",
+      role: "Software Developer",
+      imgUrl: "https://imageio.forbes.com/specials-images/imageserve/5df7fb014e2917000783339f/China-is-projected-to-surpass-the-U-S--as-the-world-s-largest-economy-in-the-next/960x0.jpg?format=jpg&width=960"
+    },
+    {
+      id: 5,
+      name: "Effie Mohamed",
+      role: "Software Developer",
+      imgUrl: "https://hrmcdn-13a1c.kxcdn.com/images/hr-sg/content-images/arina_may_2022_thailandendemic_thailandgenericshot_123rf.jpg"
+    },
+    {
+      id: 6,
+      name: "Karam Lees",
+      role: "Senior Software Developer",
+      imgUrl: "https://www.travelandleisure.com/thmb/CbvtaZu659SP8qOSCljNCuRby_Y=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/eiffel-tower-paris-france-EIFFEL0217-6ccc3553e98946f18c893018d5b42bde.jpg"
+    },
+    {
+      id: 7,
+      name: "Eleanor Dowling",
+      role: "Project Manager",
+      imgUrl: "https://cdn.kimkim.com/files/a/images/cde3310d2e51a1f1f4ac4abea78a7dfdc08075d3/big-f9c6745049c2a354190d51e6fb8c3b22.jpg"
+    },
+    {
+      id: 8,
+      name: "Haris Grey",
+      role: "Tester",
+      imgUrl: "https://media.product.which.co.uk/prod/images/original/gm-13a70770-848f-4ca6-ba92-9eb0a38b36b8-australia-travel-guideadvice-lead.jpeg"
+    },
+    {
+      id: 9,
+      name: "Sohaib Farley",
+      role: "Tester",
+      imgUrl: "https://www.visitgreece.gr/images/1743x752/jpg/files/merakos_05_santorini-oia_1743x752.webp"
+    },
+    {
+      id: 10,
+      name: "Phillip Mitchell",
+      role: "Hard Man",
+      imgUrl: "https://dynaimage.cdn.cnn.com/cnn/q_auto,w_940,c_fill,g_auto,h_529,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F170606121206-italy---travel-destination---shutterstock-517522957.jpg"
+    },
+  ]
+  export default team;
